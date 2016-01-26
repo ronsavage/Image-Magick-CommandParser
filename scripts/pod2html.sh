649889b6909cd2cbc259cae3302b43d9a7bb5357
ronsavage <ron@savage.net.au>
2016-01-26 14:40:28 +1100

    First steps to modify scripts/synopsis.pl to use Set::FA::Element

--- scripts/pod2html.sh
+++ scripts/pod2html.sh
@@ -3,11 +3,13 @@
 # $DR is my web server's doc root within Debian's RAM disk :-).
 # The latter is at /run/shm, so $DR is /run/shm/html.
 
-DIR=Perl-modules/html/Image/Magick
-FILE=CommandParser
+DIR=Perl-modules/html/Set
+FILE=FA
 
-mkdir -p $DR/$DIR ~/savage.net.au/$DIR
+mkdir -p $DR/$DIR/$FILE
+mkdir -p ~/savage.net.au/$DIR/$FILE
 
-pod2html.pl -i lib/Image/Magick/$FILE.pm -o $DR/$DIR/$FILE.html
+pod2html.pl -i lib/Set/$FILE.pm         -o ~/savage.net.au/$DIR/$FILE.html
+pod2html.pl -i lib/Set/$FILE/Element.pm -o ~/savage.net.au/$DIR/$FILE/Element.html
 
-cp -r $DR/$DIR/$FILE.html ~/savage.net.au/$DIR
+cp -r ~/savage.net.au/$DIR/* $DR/$DIR
--- scripts/synopsis.pl
+++ scripts/synopsis.pl
@@ -5,23 +5,118 @@ use strict;
 use warnings;
 use warnings qw(FATAL utf8);
 
-use File::Slurper 'read_lines';
+use Set::Array;
 
-use Image::Magick::CommandParser;
+use Set::FA::Element;
+
+my($stack) = Set::Array -> new;
 
 # ----------------------------------------------
 
-my($input_file)	= 'data/synopsis.txt';
-my($maxlevel)	= shift || 'info';
-
-for my $command (read_lines($input_file) )
+sub command
 {
-	next if ($command =~ /^\s*#/);
+	my($dfa)	= @_;
+	my($match)	= $dfa -> match;
 
-	Image::Magick::CommandParser -> new
-	(
-		command			=> $command,
-		maxlevel		=> $maxlevel,
-		print_report	=> ,1
-	) -> run;
-}
+	$stack -> push
+	({
+		token	=> $match,
+		type	=> 'command',
+	});
+
+	print "'command' matched '", $dfa -> match, "'\n";
+
+} # End of command.
+
+# ----------------------------------------------
+
+sub done
+{
+	my($dfa)	= @_;
+	my($match)	= $dfa -> match;
+
+	$stack -> push
+	({
+		token	=> $match,
+		type	=> 'done',
+	});
+
+	print "'done' matched '", $dfa -> match, "'\n";
+
+} # End of done.
+
+# ----------------------------------------------
+
+sub input_file
+{
+	my($dfa)	= @_;
+	my($match)	= $dfa -> match;
+
+	$stack -> push
+	({
+		token	=> $match,
+		type	=> 'input_file',
+	});
+
+	print "'input_file' matched '", $dfa -> match, "'\n";
+
+} # End of input_file.
+
+# ----------------------------------------------
+
+sub output_file
+{
+	my($dfa)	= @_;
+	my($match)	= $dfa -> match;
+
+	$stack -> push
+	({
+		token	=> $match,
+		type	=> 'output_file',
+	});
+
+	print "'output_file' matched '", $dfa -> match, "'\n";
+
+} # End of output_file.
+
+# ----------------------------------------------
+
+my($dfa) = Set::FA::Element -> new
+(
+	accepting	=> ['done'],
+	actions		=>
+	{
+		command =>
+		{
+			entry	=> \&command,
+		},
+		done =>
+		{
+			entry	=> \&done,
+		},
+		input_file =>
+		{
+			entry	=> \&input_file,
+		},
+		output_file =>
+		{
+			entry	=> \&output_file,
+		},
+	},
+	die_on_loop	=> 1,
+	maxlevel	=> 'debug',
+	start		=> 'start',
+	transitions	=>
+	[
+		['start',		'convert|mogrify',				'command'],
+		['command',		'[a-zA-Z][-a-zA-Z]+:',			'input_file'],
+		['command',		'[a-zA-Z][-a-zA-Z]+:[a-zA-Z]+',	'input_file'],
+		['input_file',	'.+.png',						'output_file'],
+		['output_file',	'^$',							'done'],
+		['done',		'.',							'done'],
+	],
+);
+
+$dfa -> step('convert logo:');
+
+print map{"token => $$_{token}. type => $$_{type}.\n\n"} $stack -> print;
